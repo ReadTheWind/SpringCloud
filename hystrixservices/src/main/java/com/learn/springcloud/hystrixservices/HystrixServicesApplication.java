dd597d40766228d644ffdfea5c652d53d8ded07c
liuhuan <31094897+ReadTheWind@users.noreply.github.com>
2020-04-08 17:47:12 +0800

    hystrix 初始 提交

---
+++ hystrixservices/src/main/java/com/learn/springcloud/hystrixservices/HystrixServicesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.hystrixservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixServicesApplication.class, args);
+    }
+
+}
---
+++ hystrixservices/src/test/java/com/learn/springcloud/hystrixservices/HystrixServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.hystrixservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
