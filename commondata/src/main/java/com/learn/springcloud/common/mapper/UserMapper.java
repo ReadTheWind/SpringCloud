05384cf004fda86f7b0200bb11260c2d891d3402
liuhuan <liuhuanasjob@163.com>
2020-04-28 16:20:40 +0800

    增加分环境配置

--- commondata/src/main/java/com/learn/springcloud/common/mapper/UserMapper.java
+++ commondata/src/main/java/com/learn/springcloud/common/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserMapper extends BaseMapper {
+public interface UserMapper extends BaseMapper<User> {
 
     List<User> getAllUser();
 }
